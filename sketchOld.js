5a66fac3ed566e921cdd379dd13c8c1f6333ab2b
William Martin <williammartin@gatech.edu>
2017-12-03 16:23:50 -0500

    upated look and added multiple character UI/functionality

--- sketchOld.js
+++ sketchOld.js
@@ -1,27 +1,21 @@
 function preload() {
+	bg = loadImage("./media/bg.jpg");
+	jonIcon = loadImage("./media/jon.jpg");
+	danyIcon = loadImage("./media/dany.jpg");
+	theonImg = loadImage("./media/theonRaw.jpg");
 }
 
 function setup() {
-	cnv = createCanvas(windowWidth-200,windowHeight/2);
-	cnv.id('sketch');
-	var x = (windowWidth - width) / 2;
-  	var y = 3*(windowHeight - height) / 4;
-	cnv.position(x,y);
-	bg = [28, 29, 30];
+	createCanvas(windowWidth,windowHeight);
 	background(bg);
-
-	windowHeight = cnv.height;
-	windowWidth = cnv.width;
-
-	//-------------------------
-
+	div = createDiv(CHARACTERS.THEON.info);
+	//
 	points = [];//array to hold all episodes/points
 	pointSize = 10;
 	//episodes: 63, seasons: 7
 	scaleX = 67;
 	//rating scale: 0-5;
 	scaleY = 5;
-	scalePadding = 2*(windowWidth)/scaleX;
 
 	//for squash() function
 	slope = 1;//slope of plot lines
@@ -29,13 +23,9 @@ function setup() {
 	squashed = false;
 	mousePressed = false;
 
-	//lightbox
-	viewing = false;
-
-	//--------------------------
-
+	
 	var test1 = new Point(CHARACTERS.THEON, "The Dragon and the Wolf", 67, 5, "./media/7_07theon.mp4", "Jon Snow forgives Theon.");
-	var test2 = new Point(CHARACTERS.THEON, "A Golden Crown", 9, 1, "./media/1_09theon.mp4", "Theon confronts Robb Stark.");//9
+	var test2 = new Point(CHARACTERS.THEON, "A Golden Crown", 9, 1, "./media/1_09theon.mp4", "Theon confronts Robb Stark.");
 	var test3 = new Point(CHARACTERS.THEON, "The Night Lands", 12, 3, "./media/2_02theon.mp4", "Theon retunrs home.");
 	var test4 = new Point(CHARACTERS.THEON, "What is Dead May Never Die", 13, 4, "./media/2_03theon.mp4", "Theon betrays Robb.");
 	var test5 = new Point(CHARACTERS.THEON, "The Old Gods and the New", 16, 5, "./media/2_06theon.mp4", "Theon takes Winterfell.");
@@ -45,6 +35,7 @@ function setup() {
 	var test5 = new Point(CHARACTERS.JON, "Stormborn", 1, 1, "./media/7_02theon.mp4", "Theon confronts Euron.");
 	var test5 = new Point(CHARACTERS.JON, "Stormborn", 20, 3, "./media/7_02theon.mp4", "Theon confronts Euron.");
 	var test5 = new Point(CHARACTERS.JON, "Stormborn", 57, 5, "./media/7_02theon.mp4", "Theon confronts Euron.");
+	
 }
 
 function draw() {
@@ -52,87 +43,56 @@ function draw() {
 	drawGUI();
 	drawPoints();
 	controller();
+	
+	// keyPressed();
 	checkPoints();
-	// connectPoints(points[0]);
 	if (mousePressed) {
+		// squashed = true;
 		squash();
 	}
+	charInfo();
 }
 
 function controller() {
-	var jonButton = document.getElementById("jon");
-	var danyButton = document.getElementById("dany");
-	var tyrionButton = document.getElementById("tyrion");
-	var cerseiButton = document.getElementById("cersei");
-	jonButton.onclick = function(e) {
-		CHARACTERS.JON.selected = !(CHARACTERS.JON.selected);
-	}
-	danyButton.onclick = function(e) {
-		CHARACTERS.DANY.selected = !(CHARACTERS.DANY.selected);
-	}
-	// document.getElementById("jon").onclick = function(e) {
-	// 	CHARACTERS.TYRION.selected = !(CHARACTERS.TYRION.selected);
-	// }
-	// document.getElementById("jon").onclick = function(e) {
-	// 	CHARACTERS.CERSEI.selected = !(CHARACTERS.CERSEI.selected);
-	// } 
 	if (CHARACTERS.JON.selected) {
 		connectPoints(points[0]);
-		jonButton.style.filter = "grayscale(0%)";
-	} else {
-		jonButton.style.filter = "";
 	}
-	if (CHARACTERS.DANY.selected) {
+	if (CHARACTERS.THEON.selected) {
 		connectPoints(points[1]);
-		danyButton.style.filter = "grayscale(0%)";
-	} else {
-		danyButton.style.filter = "";
-	}
-	if (CHARACTERS.TYRION.selected) {
-		// connectPoints(points[0]);
-		tyrionButton.style.filter = "grayscale(0%)";
-	} else {
-		tyrionButton.style.filter = "";
-	}
-	if (CHARACTERS.CERSEI.selected) {
-		// connectPoints(points[0]);
-		cerseiButton.style.filter = "grayscale(0%)";
-	} else {
-		cerseiButton.style.filter = "";
 	}
 }
 
 var CHARACTERS = {
-	JON: {value: 0, name: "Jon Snow", code: 'J', color: [24, 93, 95], selected: false},
+	JON: {value: 0, name: "Jon Snow", code: 'J', color: [0, 0, 255], selected: false},
 	DANY: {value: 1, name: "Daenerys Targaryen", code: 'D', color: [255, 0, 0], selected: false},
-	THEON: {value: 2, name: "Theon Greyjoy", code: 'T', color: 255, selected: false},
-	TYRION: {value: 0, name: "Tyrion Lannister", code: 'T', color: [0, 0, 255], selected: false},
-	CERSEI: {value: 0, name: "Cersei Lanniester", code: 'C', color: [0, 0, 255], selected: false},
+	THEON: {value: 2, name: "Theon Greyjoy", code: 'T', color: 255, selected: false, info: "Prince Theon Greyjoy is the only living son and heir of Lord Balon Greyjoy of the Iron Islands, and younger brother of Yara Greyjoy. Following his father's failed rebellion against the Iron Throne, Theon is taken as a hostage/ward to Lord Eddard Stark of Winterfell. After Eddard's execution in King's Landing, Theon pledges his loyalty to Eddard's eldest son, Robb Stark, in the subsequent War of the Five Kings, but ultimately betrays Robb and sides with his father in invading the North, which leads to his capture by House Bolton. He is tortured and forced into servitude by Ramsay Snow, who turns him into a broken pet named Reek. Theon, however, redeems himself by helping Ramsay's wife, Sansa Stark, escape from Winterfell and find refuge with her half-brother (revealed to be cousin) Jon Snow, who later retakes Winterfell and defeats Ramsay. Slowly returning to his former self, Theon returns to the Iron Islands, where he learns of his father's death at the hands of his own uncle, Euron Greyjoy. In retaliation, Theon flees with Yara to seek out an alliance with Daenerys Targaryen in Meereen, where they pledge House Greyjoy's forces to her. Despite abandoning Yara during the assault on the Targaryen Fleet, he later appeals to Daenerys's forces to retrieve her from Euron's custody."}
 };
 
 //draws x-axis
 function drawGUI() {
 	stroke(255);
-
-	var startX = (windowWidth-scalePadding)/(scaleX);
-	var endX = (scaleX)*(windowWidth-scalePadding)/(scaleX);
+	
+	var startX = 2*windowWidth/(scaleX+2);
+	var endX = (scaleX+1)*windowWidth/(scaleX+2);
   	line(startX, midHt, endX, midHt);
   	var tickX = startX;
   	for (var i = 0; i < scaleX; i++) {
     	line(tickX, midHt - 5, tickX, midHt + 5);
-    	// text(i+1, tickX, midHt + 15);
-    	tickX += (windowWidth-scalePadding)/(scaleX);
+    	text(i+1, tickX, midHt + 15);
+    	tickX += (windowWidth)/(scaleX+2);
   	}
+}
+
+//draws char info under x-axis
+function charInfo() {
+	if (!squashed) {
+		image(theonImg, 100, midHt+50, 300,180);
+		div.position(420, midHt+50);
+		div.style("background-color", "white");
+	} else {
+		div.hide();
+	}
 	
-	// var startX = 2*windowWidth/(scaleX+2);
-	// var endX = (scaleX+1)*windowWidth/(scaleX+2);
- //  	line(startX, midHt, endX, midHt);
- //  	var tickX = startX;
- //  	for (var i = 0; i < scaleX; i++) {
- //    	line(tickX, midHt - 5, tickX, midHt + 5);
- //    	// text(i+1, tickX, midHt + 15);
- //    	tickX += (windowWidth)/(scaleX+2);
- //  	}
 }
 
 //changes mid-height variable to screen maxY, moving all objects down screen. changes slope to flatten character lines
@@ -154,21 +114,11 @@ function squash() {
   }
 }
 
-// function keyTyped() {
+// function keyPressed() {
 //   if (keyCode == ENTER) {
-//   	lightbox();
+//     squash();
 //   }
 // }
-
-function lightbox(isViewing) {
-	if (!isViewing) {
-		viewing = true;
-		resizeCanvas(window.innerWidth-200,500);
-    	var x = (window.innerWidth - width) / 2;
-  		var y = (window.innerHeight - height) / 4;
-		cnv.position(x,y);
-	}
-}
 
 //checks if mouse is clicked on point
 function mouseClicked() {
@@ -179,7 +129,9 @@ function mouseClicked() {
 				&& mouseY < points[i].getY() + pointSize
 				&& mouseY > points[i].getY() - pointSize) {
 			mousePressed = true;
-		} 
+		} else {
+			console.log("test2");
+		}
 	}
 }
 
@@ -207,7 +159,7 @@ function Point(character, title, ep, rate, src, info) {
 	vid.hide();
 	//
 	this.getX = function() {
-		return ((this.ep)*(windowWidth-scalePadding))/(scaleX);//((this.ep+1)*windowWidth)/(scaleX+2);
+		return ((this.ep+1)*windowWidth)/(scaleX+2);
 	}
 	this.getY = function() {
 		return (midHt)-(slope*(this.rate*windowHeight)/(2*scaleY));
@@ -221,13 +173,12 @@ function Point(character, title, ep, rate, src, info) {
 			ellipse(x, y, pointSize, pointSize);
 			if (this.isMouseOver) {
 				this.infoBox.show();
-				this.infoBox.position(cnv.x + x + 15, cnv.y + y + 15);
+				this.infoBox.position(x,y-200);
 				if (mousePressed) {
-					lightbox(viewing);
 					// var vid = createVideo(this.src);
 					vid.show();
-					vid.size(width-100,height-100);
-					vid.position(cnv.x+50, cnv.y+50);
+					vid.size(windowWidth-100,windowHeight-100);
+					vid.position(25, 50);
 					vid.showControls();
 				}
 			} else { 
@@ -243,7 +194,7 @@ function Point(character, title, ep, rate, src, info) {
 //draws all points
 function drawPoints() {
 	for (var i = 0; i < points.length; i++) {
-		points[i].draw(points[i].character.selected);
+		points[i].draw(true);
 	}
 }
 
